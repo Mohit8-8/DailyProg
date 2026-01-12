6c2ed65ef9c0448d6897689c44dd7dcf6ac86965
Mohit Dev Tomar <casonpark8@gmail.com>
2026-01-12 11:08:30 +0530

    Time: 0 ms (100%), Space: 20.7 MB (36.64%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
 
-    struct CustomCompare{
-        bool operator()(int a,int b){
-            return (a % 2) < (b % 2);
-        }
-    };
+    // struct CustomCompare{
+    //     bool operator()(int a,int b){
+    //         return (a % 2) < (b % 2);
+    //     }
+    // };
     vector<int> sortArrayByParity(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // int n = nums.size();
-        // int left = 0, right = nums.size() - 1;
+        int n = nums.size();
+        int left = 0, right = nums.size() - 1;
 
-        // if(nums.size()==0) return nums;
-        // while(left < right){
-        //     if(nums[left] % 2 != 0 && nums[right] % 2 == 0 ){
-        //         swap(nums[left], nums[right]);
-        //         left++;
-        //         right--;
-        //     }
-        //     else if(nums[left]%2!=0 && nums[right]%2!=0){
-        //         right--;
-        //     }
-        // }
-        // return nums;
-
-        sort(nums.begin(),nums.end(),CustomCompare());
+        while(left <= right){
+            if(nums[left] % 2 == 1 && nums[right] % 2 == 0) swap(nums[left], nums[right]);
+            else if(nums[left] % 2 == 0 && nums[right] % 2 == 1){
+                left++;
+                right--;
+            }
+            else if(nums[left] % 2 == 1 && nums[right] % 2 == 1) right--;
+            else if(nums[left] % 2 == 0 && nums[right] % 2 == 0) left++;
+        }
         return nums;
+
+        // sort(nums.begin(),nums.end(),CustomCompare());
+        // return nums;
     }
 };
